658ab703d969b6fba3c4f6ee092685dca7069a9c
ganeshmoka <ganeshmoka1999@gmail.com>
2024-12-30 22:55:48 +0530

    infra-dev

--- 60-backend/backend.sh
+++ 60-backend/backend.sh
@@ -3,6 +3,6 @@
 component=$1
 environment=$2
 #printing the above variable.
-echo -e "component:${$component}, environment:${$environment}"
+echo -e "component:${component}, environment:${environment}"
 dnf install ansible -y 
 ansible-pull -i localhost, -U https://github.com/promptforai/expense-ansible-roles-tf.git main.yaml -e component=$component -e environment=$environment
