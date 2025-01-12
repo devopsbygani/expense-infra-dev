3ace149c98351a23bd914fbaa512241ef656bfaf
ganeshmoka <ganeshmoka1999@gmail.com>
2025-01-12 20:50:08 +0530

    expense infra

--- 20-sg/sg.tf
+++ 20-sg/sg.tf
@@ -284,8 +284,8 @@ resource "aws_security_group_rule" "frontend_vpn" {
 
 resource "aws_security_group_rule" "frontend_web_alb" {
   type              = "ingress"
-  from_port         = 22
-  to_port           = 22
+  from_port         = 80
+  to_port           = 80
   protocol          = "tcp"
   source_security_group_id = module.web_alb_sg.id
   security_group_id = module.frontend_sg.id
@@ -293,8 +293,8 @@ resource "aws_security_group_rule" "frontend_web_alb" {
 
 resource "aws_security_group_rule" "app_alb_frontend" {
   type              = "ingress"
-  from_port         = 22
-  to_port           = 22
+  from_port         = 80
+  to_port           = 80
   protocol          = "tcp"
   source_security_group_id = module.frontend_sg.id
   security_group_id = module.app_alb_sg.id
